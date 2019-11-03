64e7163a9d38a5fd4c3d89d88ca4725c60764c8f
Jonas Lundberg <josmase@users.noreply.github.com>
2019-11-03 13:22:38 +0100

    Create a animal parser

---
+++ Zoo/Parsers/AnimalParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Zoo.Parsers
+{
+    public class AnimalParser
+    {
+        public IEnumerable<Animal> ParseAnimal(XDocument document, IEnumerable<AnimalType> animalTypes)
+        {
+            var animalTypeNodes = document.Descendants("Zoo").Descendants();
+            var animals = new List<Animal>();
+            animalTypes = animalTypes.ToList();
+            
+            foreach (var typeNode in animalTypeNodes)
+            {
+                var nodeName = typeNode.Name.LocalName;
+                var animalType = animalTypes.FirstOrDefault(x => x.Animal == nodeName.Remove(nodeName.Length - 1));
+
+                foreach (var animalNode in typeNode.Descendants())
+                {
+                    var name = animalNode.Attribute("name")?.Value;
+                    var weight = animalNode.Attribute("kg")?.Value;
+                    if (name != null && weight != null)
+                    {
+                        animals.Add(new Animal(animalType, name, Convert.ToInt32(weight)));
+                    }
+                }
+            }
+
+            return animals;
+        }
+    }
+}
---
+++ ZooTests/Parsers/AnimalParserTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Xunit;
+using Zoo;
+using Zoo.Parsers;
+
+namespace ZooTests.Parsers
+{
+    public class AnimalParserTest
+    {
+        [Fact]
+        public void ShouldFindOneAnimal()
+        {
+            const string name = "name";
+            const int weight = 0;
+            const string animalTypeName = "Lion";
+            var document = new XDocument(
+                new XElement("Zoo",
+                    new XElement($"{animalTypeName}s",
+                        new XElement(animalTypeName,
+                            new XAttribute("name", name),
+                            new XAttribute("kg", weight))
+                    )));
+
+            var typeMock = new AnimalType(animalTypeName, 0, Diet.Carnivore);
+            var typesMock = new List<AnimalType>() {typeMock};
+            var animals = new AnimalParser().ParseAnimal(document, typesMock).ToList();
+            var animal = animals[0];
+            Assert.Single(animals);
+            Assert.Equal(name, animal.Name);
+            Assert.Equal(weight, animal.Weight);
+        }
+    }
+}
