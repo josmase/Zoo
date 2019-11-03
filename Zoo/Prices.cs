2ecc5e30d8be94ca5a9789e7f68c3415c1eb1e12
Jonas Lundberg <josmase@users.noreply.github.com>
2019-11-03 10:09:11 +0100

    Create a basic class to hold prices

---
+++ Zoo/Prices.cs
@@ -0,0 +1,14 @@
+﻿namespace Zoo
+{
+    public class Prices
+    {
+        public Prices(double fruits, double meat)
+        {
+            Fruits = fruits;
+            Meat = meat;
+        }
+
+        public double Meat { get; }
+        public double Fruits { get; }
+    }
+}
---
+++ ZooTests/PricesTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Zoo;
+
+namespace ZooTests
+{
+    public class PricesTest
+    {
+        [Fact]
+        public void ShouldCreatePrices()
+        {
+            const double meatPrice = 20.4;
+            const double fruitPrice = 10.2;
+
+            var prices = new Prices(fruitPrice, meatPrice);
+            
+            Assert.Equal(fruitPrice, prices.Fruits);
+            Assert.Equal(meatPrice, prices.Meat);
+        }
+    }
+}
