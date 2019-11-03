b7c28d400c8d7aa4103d97d0affcda1361c091f9
Jonas Lundberg <josmase@users.noreply.github.com>
2019-11-03 11:00:54 +0100

    Create a price parser

---
+++ Zoo/Parsers/PriceParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Zoo.Parsers
+{
+    public class PriceParser
+    {
+        private const string MeatKey = "Meat";
+        private const string FruitKey = "Fruit";
+
+        public static Prices ParsePrices(TextReader file)
+        {
+            string line;
+            double meatPrice = 0;
+            double fruitPrice = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                var priceData = line.Split("=");
+                var price = Convert.ToDouble(priceData[1]);
+                switch (priceData[0])
+                {
+                    case MeatKey:
+                        meatPrice = price;
+                        break;
+                    case FruitKey:
+                        fruitPrice = price;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Unkown price: {priceData[0]}");
+                }
+            }
+
+            return new Prices(fruitPrice, meatPrice);
+        }
+    }
+}
---
+++ ZooTests/Parsers/PriceParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using Zoo.Parsers;
+
+namespace ZooTests.Parsers
+{
+    public class PriceParserTest
+    {
+        [Fact]
+        public void ShouldGetPrices()
+        {
+            const double meatPrice = 12.3;
+            const double fruitPrice = 2.3;
+            var priceString = $@"Meat={meatPrice}
+Fruit={fruitPrice}";
+            var reader = new StringReader(priceString);
+            var prices = PriceParser.ParsePrices(reader);
+
+            Assert.Equal(fruitPrice, prices.Fruits);
+            Assert.Equal(meatPrice, prices.Meat);
+        }
+
+        [Fact]
+        public void ShouldThrowWExceptionForInvalidKey()
+        {
+            const string invalidKey = "invalid";
+            var priceString = $@"{invalidKey}=123
+Fruit=123";
+            var reader = new StringReader(priceString);
+            Assert.Throws<ArgumentOutOfRangeException>(() => PriceParser.ParsePrices(reader));
+        }
+    }
+}
