b1b7613fb6195e706fb7cbda47d4efab70966289
Jonas Lundberg <josmase@users.noreply.github.com>
2019-11-03 15:32:04 +0100

    Add price calculation test

--- ZooTests/AnimalTypeTest.cs
+++ ZooTests/AnimalTypeTest.cs
@@ -36,5 +36,24 @@ namespace ZooTests
             Assert.Equal(ratio, type.Ratio);
             Assert.Equal(0, type.MeatPercent);
         }
+
+        [Theory]
+        [InlineData(10, 0.4, 0.5, Diet.Omnivore, 10)]
+        [InlineData(16, 0.4, 0.5, Diet.Omnivore, 16)]
+        [InlineData(10, 0.4, 0.4, Diet.Omnivore, 9.6)]
+        [InlineData(10, 0.1, 0.4, Diet.Omnivore, 2.4)]
+        [InlineData(10, 0.4, 0, Diet.Carnivore, 12)]
+        [InlineData(16, 0.4, 0, Diet.Carnivore, 19.2)]
+        [InlineData(10, 0.4, 0, Diet.Herbivore, 8)]
+        [InlineData(16, 0.4, 0, Diet.Herbivore, 12.8)]
+        [InlineData(16000, 0.4, 0, Diet.Herbivore, 12800)]
+        [InlineData(10, 0.33333, 0, Diet.Herbivore, 6.667)]
+        public void ShouldCalculatePrice(int weight, double ratio, double meatPercent, Diet diet, double expected)
+        {
+            var prices = new Prices(2, 3);
+            var type = new AnimalType("", ratio, diet, meatPercent);
+            var price = type.CalculatePrice(weight, prices);
+            Assert.Equal(expected, price);
+        }
     }
 }
